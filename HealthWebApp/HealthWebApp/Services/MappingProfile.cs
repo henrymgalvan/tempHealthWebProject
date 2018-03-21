3af22497f43a9dd1e46e156f88952a9381fb2c23
henrymgalvan <hgalvan1971@yahoo.com>
2018-03-21 13:54:47 +0800

    remove blank line

--- HealthWebApp/HealthWebApp/Services/MappingProfile.cs
+++ HealthWebApp/HealthWebApp/Services/MappingProfile.cs
@@ -23,7 +23,6 @@ namespace HealthWebApp.Services
                 .ForMember(dest => dest.City, opt => opt.MapFrom(src => src.HouseholdMember.HouseholdProfile.Barangay.CityMunicipality.Name.ToString()))
                 .ForMember(dest => dest.Province, opt => opt.MapFrom(src => src.HouseholdMember.HouseholdProfile.Barangay.CityMunicipality.Province.Name.ToString()))
                 .ForMember(dest => dest.Region, opt => opt.MapFrom(src => src.HouseholdMember.HouseholdProfile.Barangay.CityMunicipality.Province.Region.Name.ToString()))
-
                 .ForMember(dest => dest.PhilHealthID, opt => opt.MapFrom(src => src.PhilHealth.PhilHealthID))
                 .ForMember(dest => dest.Religion, opt => opt.MapFrom(src => src.Religion.LongName))
                 .ForMember(dest => dest.Father, opt => opt.MapFrom(src => src.Father.FullName))
